117db1e91e38a453853e1a0fc0184886326fa436
Marta <marta.rozycka@student.kuleuven.be>
2023-05-12 16:16:04 +0200

    creating new activities

---
+++ app/src/main/java/be/kuleuven/gt/myapplication/MyProfileActivity.java
@@ -0,0 +1,14 @@
+package be.kuleuven.gt.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+}
