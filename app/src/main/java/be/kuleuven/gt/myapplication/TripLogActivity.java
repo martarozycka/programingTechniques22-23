82d3382b733c29f20a9c245fafc4031496d55149
Marta <marta.rozycka@student.kuleuven.be>
2023-05-21 20:18:41 +0200

    spinner contains locations per trip

--- app/src/main/java/be/kuleuven/gt/myapplication/TripLogActivity.java
+++ app/src/main/java/be/kuleuven/gt/myapplication/TripLogActivity.java
@@ -7,6 +7,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import be.kuleuven.gt.model.Comment;
 import be.kuleuven.gt.model.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,12 +37,14 @@ public class TripLogActivity extends AppCompatActivity {
     private TextView txtStartDate;
     private TextView txtEndDate;
     private TextView txtLocationName;
-
     private static final String LOCATION_URL = "https://studev.groept.be/api/a22pt303/selectLocationsPerTrip/";
-
     private RecyclerView commentView;
     private List<Comment> comments = new ArrayList<>();
     private static final String COMMENT_URL = "https://studev.groept.be/api/a22pt303/selectCommentsPerLocation/";
+    private Spinner spLocation;
+    private ArrayList<String> locationNamesList = new ArrayList<>();
+    private String selectLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,13 +63,51 @@ public class TripLogActivity extends AppCompatActivity {
         txtStartDate.setText(trip.getStartDate());
         txtEndDate.setText(trip.getEndDate());
 
+        spLocation = findViewById(R.id.spLocation);
+
+
         requestLocations();
+
+
+
+// Create an ArrayAdapter using the location data and a default layout
+        ArrayAdapter<String> locationAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, locationNamesList);
+
+// Specify the layout to use when the choices appear
+        locationAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+// Set the adapter for the Spinner
+        spLocation.setAdapter(locationAdapter);
+        //spLocation.setSelection(locationAdapter.getPosition(simple_spinner_item));
+
+
 
 //        CommentAdapter adapter = new CommentAdapter(comments);
 //        commentView.setAdapter( adapter );
 //        commentView.setLayoutManager( new LinearLayoutManager( this ));
 
+        // Add an OnItemSelectedListener to the spinner
+        spLocation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Get the selected location from the spinner
+//                String selectedLocationName = (String) parent.getItemAtPosition(position);
+//
+//                // Update the location TextView with the selected location
+//                txtLocationName.setText(selectedLocationName);
+//                requestComments(selectedLocationName);
+//                // Initialize the comment adapter and set it to the RecyclerView
+//                CommentAdapter adapter = new CommentAdapter(comments);
+//                commentView.setAdapter(adapter);
+//                commentView.setLayoutManager(new LinearLayoutManager(TripLogActivity.this));
+                spLocation.setSelection(position);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Handle the case where nothing is selected (if needed)
+            }
+        });
     }
 
     private void requestLocations() {
@@ -80,7 +124,9 @@ public class TripLogActivity extends AppCompatActivity {
 
                         // initialize 1st location of the trip
                         if (!locationList.isEmpty()) {
-                            txtLocationName.setText(locationList.get(0).getLocationName());
+                            //txtLocationName.setText(spLocation.getSelectedItem().toString());
+                          txtLocationName.setText(locationList.get(0).getLocationName());
+                            //txtLocationName.setText(selectLocation);
                         }
 
                         requestComments();
@@ -110,6 +156,7 @@ public class TripLogActivity extends AppCompatActivity {
                 String latitude =jsonObject.getString("latitude");
                 Location location = new Location(locationName, longitude, latitude);
                 locationList.add(location);
+                locationNamesList.add(locationName);
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -118,9 +165,9 @@ public class TripLogActivity extends AppCompatActivity {
 
     //for the comments!
     private void requestComments() {
-        String locationName = null;
+       String locationName = null;
         if (!locationList.isEmpty()) {
-            locationName = locationList.get(0).getLocationName();
+           locationName = locationList.get(0).getLocationName();
         }
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, COMMENT_URL + locationName, null,
